c982d581336513d526ba3c140529e1a136f8bb9a
Ursula Borrego <ursulaborrego@hotmail.com>
2022-10-31 10:34:25 +0000

    stop at values race bad

--- main.js
+++ main.js
@@ -8,14 +8,31 @@ const btn = document.querySelector('.js-btn');
 //Resultado
 const result = document.querySelector('.js-result');
 
-//VARIABLES
+//Const razas buenas
+const raceGood1 = document.querySelector('.race-good1');
+const raceGood2 = document.querySelector('.race-good2');
+const raceGood3 = document.querySelector('.race-good3');
+const raceGood4 = document.querySelector('.race-good4');
+const raceGood5 = document.querySelector('.race-good5');
 
+//Const razas malvadas
+const raceBad1 = 'Sureños malos'; //2
+const raceBad2 = 'Orcos'; //2
+const raceBad3 = 'Goblins'; //2
+const raceBad4 = 'Huargos'; //3
+const raceBad5 = 'Trolls'; //5
 
 //FUNCIONES
 //Función para generar un número aleatorio
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
-}
+};
+//Función para comparar valores de razas buenas y malas
+function getResult() {
+    if{
+        
+    }
+};
 
 //ADDEVENTLISTENER
 //Llamada al botón

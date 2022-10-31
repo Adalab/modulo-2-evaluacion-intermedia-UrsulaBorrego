d15d1a7a0fc59b53460c334c6c40e4cddf2f0edf
Ursula Borrego <ursulaborrego@hotmail.com>
2022-10-31 09:37:55 +0000

    create querySelector and addListener btn

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
 'use strict';
 
 //QUERYSELECTOR
+//Select
+const chooseRace = document.querySelector('.js-choose-race');
+//Botón
+const btn = document.querySelector('.js-btn');
+//Resultado
+const result = document.querySelector('.js-result');
 
 //VARIABLES
 
+
 //FUNCIONES
+//Función para generar un número aleatorio
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+}
 
 //ADDEVENTLISTENER
+//Llamada al botón
+btn.addEventListener("click", (event) =>{
+    event.preventDefault();
+});
 
 //LLAMADAS A LAS FUNCIONES
